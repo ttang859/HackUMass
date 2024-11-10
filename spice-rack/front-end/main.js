fce100f646712b32355ffb9b25c957c9288f4a08
Tyler Tang <125093516+ttang859@users.noreply.github.com>
2024-11-10 10:02:58 -0500

    Delete spice-rack directory

--- spice-rack/front-end/main.js
+++
@@ -1,9 +0,0 @@
-import { Nicholas } from "./src/nonchalant-nicholas.js";
-const container = document.getElementById("app");
-
-//make class to do html components
-//do this dynamically since we don't know how many spice spaces we have
-const spiceRack = ["salt", "pepper", "garlic powder", "onion powder", "paprika"];
-const newNick = new Nicholas(spiceRack);
-container.innerHTML = "";
-container.appendChild(newNick.render())
